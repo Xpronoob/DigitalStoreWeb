8fd41768fb612cb8c3c3bdd7538a47cd3593dbaa
Xpronoob <kevin_03_puris@hotmail.com>
2024-11-02 13:10:59 -0600

    AuthGuard in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import PublicLayout from './layouts/PublicLayout/PublicLayout';
 import RoutesWithNotFound from './utils/RoutesWithNotFound.utility';
+import { AuthGuard } from './guards/auth.guard';
 
 const Index = lazy(() => import('./pages/Public/Index/Index'));
 const Login = lazy(() => import('./pages/Public/Login/Login'));
 const Register = lazy(() => import('./pages/Public/Register/Register'));
+
+const Profile = lazy(() => import('./pages/Private/Profile/Profile'));
 
 function App() {
   return (
@@ -18,6 +21,10 @@ function App() {
             <Route index element={<Index />} />
             <Route path={'/login'} element={<Login />} />
             <Route path={'/register'} element={<Register />} />
+          </Route>
+          {/* AUTHENTICATED */}
+          <Route element={<AuthGuard privateValidation={true} />}>
+            <Route path={'/private/*'} element={<Profile />} />
           </Route>
         </RoutesWithNotFound>
       </BrowserRouter>
