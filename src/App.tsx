f5761545d8b7921e6f8007262c22cbd5ddc66e2e
Xpronoob <kevin_03_puris@hotmail.com>
2024-11-03 23:17:40 -0600

    RoleGuard implementation in Admin Routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import PublicLayout from './layouts/PublicLayout/PublicLayout';
 import RoutesWithNotFound from './utils/RoutesWithNotFound.utility';
 import { AuthGuard } from './guards/auth.guard';
+import RoleGuard from './guards/role.guard';
 
 const Index = lazy(() => import('./pages/Public/Index/Index'));
 const Login = lazy(() => import('./pages/Public/Login/Login'));
@@ -25,6 +26,14 @@ function App() {
           {/* AUTHENTICATED */}
           <Route element={<AuthGuard privateValidation={true} />}>
             <Route path={'/private/*'} element={<Profile />} />
+          </Route>
+          {/* ADMIN */}
+          <Route element={<RoleGuard roles={['dev', 'admin']} />}>
+            <Route element={<RoleGuard roles={['user']} />}>
+              <Route path={'/admin/users/*'} element={<h1>AdminUsers</h1>} />
+            </Route>
+
+            <Route path={'/admin/*'} element={<h1>Admin</h1>} />
           </Route>
         </RoutesWithNotFound>
       </BrowserRouter>
