eeb7d16dcaf402b35103e7ad0d41e521980a7057
Xpronoob <kevin_03_puris@hotmail.com>
2024-11-05 12:49:57 -0600

    Form disabled & checked attributes

--- src/components/ui/InputForm.tsx
+++ src/components/ui/InputForm.tsx
@@ -6,6 +6,7 @@ interface Props<T extends FieldValues> {
   label: string;
   type?: string;
   error?: FieldError;
+  disabled?: boolean;
 }
 
 const InputForm = <T extends FieldValues>({
@@ -13,7 +14,8 @@ const InputForm = <T extends FieldValues>({
   control,
   label,
   type,
-  error
+  error,
+  disabled = false
 }: Props<T>) => {
   return (
     <div className='form-group'>
@@ -26,7 +28,13 @@ const InputForm = <T extends FieldValues>({
             id={String(fieldKey)}
             type={type}
             {...field}
-            className={`form-control ${error ? 'is-invalid' : ''}`}
+            className={`form-control ${
+              error ? 'is-invalid' : ''
+            } dark:bg-gray-800 m-1 p-1 ${
+              disabled ? 'bg-gray-200 dark:bg-gray-600' : ''
+            }`}
+            checked={field.value}
+            disabled={disabled}
           />
         )}
       />
